1163261fddec12c9ee5c16337d1fa92715428d90
Wrg <xcomwrg@gmail.com>
2017-07-17 14:11:20 +0800

    ver1.0

---
+++ RangeId/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace RangeId
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bid = textBox1.Text;
+            string did = textBox2.Text;
+            //bid = "330327199012";
+            //did = "6812";
+            // 330327199012266812
+            int year = Convert.ToInt32(bid.Substring(bid.Length-6, 4));
+            int month = Convert.ToInt32(bid.Substring(bid.Length-2, 2));
+            for (int i = 1; i <= DateTime.DaysInMonth(year, month); i++)
+            {
+                string id = bid + i.ToString().PadLeft(2, '0') + did;
+                if (CheckIDCard18(id))
+                {
+                    MessageBox.Show(id+", 已复制到剪辑版");
+                    Clipboard.SetDataObject(id);
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 18位身份证验证
+        /// </summary>
+        /// <param name="Id">身份证号</param>
+        /// <returns></returns>
+        private bool CheckIDCard18(string Id)
+        {
+            long n = 0;
+            if (long.TryParse(Id.Remove(17), out n) == false || n < Math.Pow(10, 16) || long.TryParse(Id.Replace('x', '0').Replace('X', '0'), out n) == false)
+            {
+                return false;//数字验证
+            }
+            string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+            if (address.IndexOf(Id.Remove(2)) == -1)
+            {
+                return false;//省份验证
+            }
+            string birth = Id.Substring(6, 8).Insert(6, "-").Insert(4, "-");
+            DateTime time = new DateTime();
+            if (DateTime.TryParse(birth, out time) == false)
+            {
+                return false;//生日验证
+            }
+            string[] arrVarifyCode = ("1,0,x,9,8,7,6,5,4,3,2").Split(',');
+            string[] Wi = ("7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2").Split(',');
+            char[] Ai = Id.Remove(17).ToCharArray();
+            int sum = 0;
+            for (int i = 0; i < 17; i++)
+            {
+                sum += int.Parse(Wi[i]) * int.Parse(Ai[i].ToString());
+            }
+            int y = -1;
+            Math.DivRem(sum, 11, out y);
+            if (arrVarifyCode[y] != Id.Substring(17, 1).ToLower())
+            {
+                return false;//校验码验证
+            }
+            return true;//符合GB11643-1999标准
+        }
+    }
+}
